789a058273e3d6ea3083ca7450c9d347e8e12208
bogdanovicb <b.bogdanovic@case-3d.com>
2021-07-30 16:59:22 +0200

    Added function to convert from roman to arabic.

--- roman_numeral_converter.js
+++ roman_numeral_converter.js
@@ -1,4 +1,4 @@
-// Function for repeating the string for a given multiplier
+// Repeat the string for a given multiplier
 function stringRepeat(str, multiplier) {
     let result = "";
 
@@ -9,7 +9,52 @@ function stringRepeat(str, multiplier) {
     return result;
 }
 
-// Function for converting arabic numbers to roman numbers
+// Convert the character to number
+function convertCharacterToNumber(char) {
+    switch (char) {
+        case "I":
+            return 1;
+        case "V":
+            return 5;
+        case "X":
+            return 10;
+        case "L":
+            return 50;
+        case "C":
+            return 100;
+        case "D":
+            return 500;
+        case "M":
+            return 1000;
+        default:
+            return 0;
+    }
+}
+
+// Function for converting roman to arabic numbers
+function convertToArabic(str) {
+    let result = 0,
+    currentNumber = 0,
+    previousNumber = 0;
+    str = str.toUpperCase();
+
+    for (let i = 0; i < str.length; i++){
+        // Convert the character to number
+        currentNumber = convertCharacterToNumber(str[i]);
+        // If the current number is higher than previous then subtract it twice with previous
+        if (currentNumber > previousNumber) {
+          currentNumber = currentNumber - previousNumber * 2;
+        }
+        // Calculate the result by adding the current number
+        result += currentNumber;
+        // When calculation is done the previous number is equal to current number
+        previousNumber = currentNumber;
+    }
+
+    return result;
+}
+
+// Function for converting arabic to roman numbers
 function convertToRoman(num) {
     let numbers = [
         {
@@ -68,19 +113,22 @@ function convertToRoman(num) {
         result = "";
 
     for (let i = numbers.length - 1; i >= 0; i--) {
-        // Calculate the quotent by rounding it to the floor
-        let quotent = Math.floor(num / numbers[i].value);
+        
+// Calculate the quotient by rounding it to the floor
+        let quotient = Math.floor(num / numbers[i].value);
         // Create the result by adding the strings using the custom function for a string repeat
-        result += stringRepeat(numbers[i].roman, quotent);
+        result += stringRepeat(numbers[i].roman, quotient);
 
         // Create the result by adding the strings using the string.repeat() function. ES2015
-        // result += numbers[i].roman.repeat(quotent);
+        // result += numbers[i].roman.repeat(quotient);
 
-        // Reduce the number by subtracting the (quotent * numbers[i].value)
-        num -= quotent * numbers[i].value;
+        // Reduce the number by subtracting the (quotient * numbers[i].value)
+        num -= quotient * numbers[i].value;
     }
 
     return result;
 }
 
-console.log(convertToRoman(4));
+console.log(convertToRoman(3999));
+
+console.log(convertToArabic("MMMCMXCIX"));
